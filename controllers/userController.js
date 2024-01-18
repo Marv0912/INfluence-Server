abf70842c1f55fc2f398c3641b9cf9ecb1d8fd01
Marv0912 <marvin.fajardo.s@icloud.com>
2024-01-18 16:39:19 -0400

    get, update, delete, user controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,40 @@
 const User = require('../models/User')
+const Company = require('../models/Company')
+const Influencer = require('../models/Influencer')
 
 // fetch data for user details page when wanting to edit profile
 exports.getUserProfile = (req, res, next) => {
-    User.findById(req.params.userId)
+    User.findById(req.params.userId).select("-password")
         .then((user) => {
             if (!user) {
                 return res.status(404).json({ message: 'User not found' })
             }
-            const { name, username, email, role, instagramUrl, photo } = user;
+            console.log('User Role:', user.role);
+            let userData = { ...user.toObject() };
 
-            const fetchedUserInfo = {
-                name,
-                username,
-                email,
-                role,
-                instagramUrl,
-                photo,
+            if (user.role === 'influencer') {
+                Influencer.findOne({ user: req.params.userId })
+                    .then((influencer) => {
+                        userData.influencer = influencer;
+                        console.log('Company Data:', influencer);
+                        res.status(200).json(userData);
+                    })
+                    .catch((error) => {
+                        console.error(error)
+                        res.status(500).json({ message: 'Internal Server Error' })
+                    })
+            } else if (user.role === 'company') {
+                Company.findOne({ user: req.params.userId })
+                    .then((company) => {
+                        userData.company = company
+                        console.log('Company Data:', company);
+                        res.status(200).json(userData)
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                        res.status(500).json({ message: 'Internal Server Error' })
+                    })
             }
-            res.status(200).json(fetchedUserInfo)
         })
 
         .catch((error) => {
@@ -32,21 +49,31 @@ exports.updateUserProfile = (req, res, next) => {
     User.findByIdAndUpdate(req.params.userId, updatedData, { new: true })
         .then((updatedUser) => {
             if (!updatedUser) {
-                return res.status(404).json({ message: 'User not found'})
+                return res.status(404).json({ message: 'User not found' })
             }
-            res.status(200).json({ message: 'User profile updated successfully', user: updatedUser})
+            res.status(200).json({ message: 'User profile updated successfully', user: updatedUser })
         })
         .catch((error) => {
             console.error(error);
-            res.status(500).json({ message: 'Internal Server Error'})
+            res.status(500).json({ message: 'Internal Server Error' })
         })
 }
 exports.deleteUserProfile = (req, res, next) => {
     User.findByIdAndDelete(req.params.userId)
         .then((deletedUser) => {
             if (!deletedUser) {
-                return res.status(404).json({ message: 'User not found'})
+                return res.status(404).json({ message: 'User not found' });
             }
-            res.status(200).json({ message: 'Successfully deleted User account'})
+            if (deletedUser.role === 'influencer') {
+                return Influencer.findOneAndRemove({ user: req.params.userId });
+            } else if (deletedUser.role === 'company') {
+                return Company.findOneAndRemove({ user: req.params.userId });
+            }
         })
-}
+        .then(() => {
+            res.status(200).json({ message: 'User deleted successfully' });
+        })
+        .catch((error) => {
+            res.status(500).json({ message: 'Internal Server Error' });
+        });
+};
